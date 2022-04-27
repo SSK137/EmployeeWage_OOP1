2ea86e0056fb33219211fe2f3cd2ee58dfffafee
SSK137 <saurabhkodam@gmail.com>
2022-04-27 13:17:10 +0530

    EmployeeWage: Employee Attendance

--- src/EmployeeWage_OOP1.java
+++ src/EmployeeWage_OOP1.java
@@ -1,5 +1,17 @@
+import java.util.*;
 public class EmployeeWage_OOP1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Using OOP Concepts");
+
+        Random rdm = new Random();          //rdm is veriable name for Random function
+        int present = rdm.nextInt(2);
+
+        //Checking Employee is present or not
+        if(present==1){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
