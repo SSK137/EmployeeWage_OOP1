1c4bb1511a0b77147e0a4ca71718438edefe69b4
SSK137 <saurabhkodam@gmail.com>
2022-04-27 13:33:54 +0530

    EmployeeWageOOP1: Daily Wage

--- src/EmployeeWage_OOP1.java
+++ src/EmployeeWage_OOP1.java
@@ -5,13 +5,16 @@ public class EmployeeWage_OOP1 {
 
         Random rdm = new Random();          //rdm is veriable name for Random function
         int present = rdm.nextInt(2);
+        int wagePerHR=20;
+        int FullDayHR=8;
+        int DailyWage= present*wagePerHR*FullDayHR;
 
         //Checking Employee is present or not
         if(present==1){
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present So Daily Wage is :"+DailyWage);
         }
         else {
-            System.out.println("Employee is absent");
+            System.out.println("Employee is absent So Daily Wage is :"+DailyWage);
         }
     }
 }
